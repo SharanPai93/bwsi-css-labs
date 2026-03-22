2e05b76a22e3a405ee281adb5cb4d4f1ac89eb07
Sharan Pai <sharan3.pai@gmail.com>
2026-03-22 12:00:32 -0700

    Create tests_1d.py

---
+++ tests/tests_1d.py
@@ -0,0 +1,20 @@
+"""
+tests_1d.py
+
+This module contains unit tests for the two_sum function defined in lab_1d.py.
+"""
+
+import pytest
+from labs.lab_1.lab_1d import two_sum
+
+def test_valid():
+    assert two_sum([1, 4, 2, 5], 6) == [0, 3] or two_sum([1, 4, 2, 5]) == [1, 2]
+    assert two_sum([1,2,3,4,5,6], 11) == [4, 5]
+    assert two_sum([3, 9, 10, 2, 5], 11) == [1, 3]
+
+def test_invalid():
+    assert two_sum([2, 5, 3, 6], 100) == []
+    assert two_sum([5, 10, 15], 5) == []
+
+if __name__ == "__main__":
+    pytest.main()
